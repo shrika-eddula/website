ba1bb4c60b58ca4a380b0f83fb4f0c7a65b97606
SEddula <shrika@mit.edu>
2025-01-21 11:09:16 -0800

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
   },
   build: {
     outDir: 'dist',
